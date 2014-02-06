38483115e9f50f6fa5e618280ebf388c27f17ed1
Greg Ferguson <8si.greg@gmail.com>
2014-02-05 22:04:40 -0700

    added #! line

--- get_instruments.rb
+++ get_instruments.rb
@@ -5,16 +5,15 @@ require 'typhoeus'
 require 'rss/maker'
 
 URL     = 'http://phoenix.craigslist.org/search/msg?query=amplifier&srchType=A'
-TARGETS = ['rivera', 'egnater', 'blackstar', 'mesa boogie', 'lonestar', 'les paul', 'carvin' ]
+TARGETS = ['rivera', 'mesa boogie', 'transatlantic', 'stratocaster', 'fender' 'les paul', 'carvin' ]
 
 RSS_VERSION = '2.0'
 
-
 rss_content = RSS::Maker.make(RSS_VERSION) do |_rss|
 
-  _rss.channel.title       = "Craigslist instruments: #{Time.now}"
+  _rss.channel.title       = "Craigslist instruments: #{ Time.now }"
   _rss.channel.link        = URL
-  _rss.channel.description = 'Musical instruments found after searching for: ' << (ARGV | TARGETS).join(', ')
+  _rss.channel.description = 'Musical instruments found after searching for: %s' % (ARGV | TARGETS).join(', ')
   _rss.items.do_sort       = true
 
   (ARGV.map{ |_i| _i.downcase } + TARGETS).each do |_target|
@@ -25,9 +24,9 @@ rss_content = RSS::Maker.make(RSS_VERSION) do |_rss|
       desc = _result.inner_text.tr(" \t\r\n", ' ').squeeze(' ').strip[2 .. -1]
 
       item = _rss.items.new_item
-      item.title = "\"#{_target}\" --> #{desc[/ - (.+)$/, 1]}"
+      item.title = "\"#{ _target }\" --> #{ desc[/ - (.+)$/, 1] }"
       item.link  = _result.at('a').attr('href')
-      item.date  = Time.parse(desc[0 .. 5])
+      item.date  = Time.parse(desc[0 .. 5]) rescue Time.now
     end
   end
 end
